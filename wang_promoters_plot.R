23548618a4878cda45e45784a5b18ebef88d7e97
ppreshant <ppresh.ant@gmail.com>
2021-01-10 18:01:17 -0600

    First commit with gitignore and wang_promoters_plot.R

---
+++ wang_promoters_plot.R
@@ -0,0 +1,13 @@
+# Ad hoc script : Get and plot harris wang promoter-rbs efficacy across organisms from the paper
+
+# Author: Prashant, # Date: 10/Jan/2021
+
+
+# preliminaries ----
+
+# Loading libraries, functions and user inputs
+source('./general_functions.R') # Source the general_functions file
+
+
+# Data import ----
+
